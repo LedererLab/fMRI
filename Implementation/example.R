6e9a0d1d50ace9084f3b49bdd09d4e80be2d9981
johanneslederer <johanneslederer@mail.de>
2017-10-26 11:43:09 -0700

    Adjusted file names in example code.

--- Implementation/example.R
+++ Implementation/example.R
@@ -1,7 +1,7 @@
 require(graphics); require(grDevices)
 
-fMRI_Data <- read.csv( "./TestData/test_fMRI.csv" , header=TRUE, sep=",")
-Distance_Data <- read.table( "./TestData/distance_matrix.csv" )
+fMRI_Data <- read.csv( "./ExampleData/example_fMRI.csv" , header=TRUE, sep=",")
+Distance_Data <- read.table( "./ExampleData/distance_matrix.csv" )
 
 fMRI_Data <- as.matrix( fMRI_Data[,-1] )
 Distance_Data <- as.matrix( Distance_Data )
